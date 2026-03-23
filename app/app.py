d514a1d7acd8946f3332a3e2ac9480c660d0f761
Anji Babu (LTIMINDTREE LIMITED) <v-anjibabu@microsoft.com>
2026-03-23 11:20:25 +0530

    testing the flask app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello DevSecOps!"
+def home():
+    return "App is working 🚀"
+
+@app.route("/health")
+def health():
+    return "OK"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-    
+    app.run(host="0.0.0.0", port=6000)
